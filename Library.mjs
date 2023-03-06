85687cae894840b269710521331994e51bab5c74
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-06 11:47:03 -0300

    Create Library.mjs

---
+++ Library.mjs
@@ -0,0 +1,26 @@
+/**
+*  Library
+*  En esta clase el usuario podra interactuar
+*  @example const instance = new Object()
+**/
+
+class Library{
+  
+    #libName;
+    #libAddress;
+    #libRoom;
+    #libTable;
+
+    constructor(name, address, rooms, tables){
+        this.#libName;
+        this.#libAddress;
+        this.#libRoom;
+        this.#libTable;
+    }
+  
+    sell(){};
+    dwell(){};
+    exhibit(){};
+    receive(){};
+    lend(){}; 
+}
