c3d7e12e7dce560a0b3cf7f38dfd1e9a7e35a995
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-29 12:14:46 -0300

    Create books.js

---
+++ books.js
@@ -0,0 +1,36 @@
+// Arreglo de libros ya definidos en un archivo JSON
+
+const booksJSON = [
+    {
+        title: "Steel Ball Run",
+        writer: "Hirohiko Araki",
+        published: 2004,
+        gender: "manga",
+        stock: 1
+    }, {
+        title: "Veinte mil leguas de viaje submarino",
+        writer: "Julio Verne",
+        published: 1869,
+        gender: "novel",
+        stock: 2
+    }, {
+        title: "Los juegos del hambre Sinsajo",
+        writer: "Suzanne Collins",
+        published: 2008,
+        gender: "sci-fi",
+        stock: 3
+    }, {
+        title: "El ultimo heroe del Olimpo",
+        writer: "Rick Riordan",
+        published: 2009,
+        gender: "greek mythology",
+        stock: 4
+
+    }, {
+        title: "The Maze Runner",
+        writer: "James Dashner",
+        published: 2009,
+        gender: "young adult fiction",
+        stock: 5
+    },
+];
