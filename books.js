d5cc46a66265a0ab51e97819c1ca764d9de6dbb7
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-04-04 19:21:20 -0300

    Update books.js

--- books.js
+++ books.js
@@ -1,41 +1,41 @@
 // Arreglo de libros
 
 export const listBooks = [
-    {
-        id: 1,
-        title: "Steel Ball Run",
-        writer: "Hirohiko Araki",
-        published: 2004,
-        gender: "manga",
-        stock: 1
-    }, {
-        id: 2,
-        title: "Veinte mil leguas de viaje submarino",
-        writer: "Julio Verne",
-        published: 1869,
-        gender: "novel",
-        stock: 2
-    }, {
-        id: 3,
-        title: "Los juegos del hambre Sinsajo",
-        writer: "Suzanne Collins",
-        published: 2008,
-        gender: "sci-fi",
-        stock: 3
-    }, {
-        id: 4,
-        title: "El ultimo heroe del Olimpo",
-        writer: "Rick Riordan",
-        published: 2009,
-        gender: "greek mythology",
-        stock: 4
+        {
+            id: 1,
+            title: "Steel Ball Run",
+            writer: "Hirohiko Araki",
+            published: 2004,
+            gender: "manga",
+            stock: 1
+        }, {
+            id: 2,
+            title: "Veinte mil leguas de viaje submarino",
+            writer: "Julio Verne",
+            published: 1869,
+            gender: "novel",
+            stock: 2
+        }, {
+            id: 3,
+            title: "Los juegos del hambre Sinsajo",
+            writer: "Suzanne Collins",
+            published: 2008,
+            gender: "sci-fi",
+            stock: 3
+        }, {
+            id: 4,
+            title: "El ultimo heroe del Olimpo",
+            writer: "Rick Riordan",
+            published: 2009,
+            gender: "greek mythology",
+            stock: 4
 
-    }, {
-        id: 5,
-        title: "The Maze Runner",
-        writer: "James Dashner",
-        published: 2009,
-        gender: "young adult fiction",
-        stock: 5
-    },
+        }, {
+            id: 5,
+            title: "The Maze Runner",
+            writer: "James Dashner",
+            published: 2009,
+            gender: "young adult fiction",
+            stock: 0
+        },
 ];
